32a2f45e95e8cf8fb46dec42de2f3ada3a32b195
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2023-08-28 22:20:29 +0330

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import json
+import re
 
 # Read IP and port from the "ip_port_list.txt" file
 try:
@@ -27,12 +28,8 @@ if len(sys.argv) < 2:
     print("Usage: python my-script.py metric-file.txt,metric-file.txt ...")
     sys.exit(1)
 
-input_files = sys.argv[1]
-metric_files = input_files.split(',')
+metric_files = sys.argv[1].split(',')
 processed_files = []  # Store processed file names
-
-# Create a comma-separated string of input file names
-input_file_names = ",".join(metric_files)
 
 for file_name in metric_files:
     file_name = file_name.strip()
@@ -42,7 +39,7 @@ for file_name in metric_files:
         with open(file_name, 'r') as file:
             for line in file:
                 measurement = line.strip()
-                if '*' in measurement:
+                if re.search(r'\\w\*', measurement):
                     process_file = True
                     break
     except FileNotFoundError:
@@ -53,36 +50,48 @@ for file_name in metric_files:
         unique_measurement_names = set()  # Store unique measurement names for each file
         try:
             with open(file_name, 'r') as file:
-                for line in file:
+                lines = file.readlines()  # Read all lines
+                new_lines = []  # Store lines to overwrite the file
+                
+                for line in lines:
                     measurement = line.strip()
-                    for host in host_names:
-                        query = f'SELECT max("value") FROM /"{measurement}/" WHERE ("host" =~ /^{host}$/) AND time >= now() - 10s AND time <= now() GROUP BY time(10s) fill(none)'
+                    if re.search(r'\\w\*', measurement):
+                        for host in host_names:
+                            query = f'SELECT max("value") FROM /"{measurement}/" WHERE ("host" =~ /^{host}$/) AND time >= now() - 10s AND time <= now() GROUP BY time(10s) fill(none)'
 
-                        # Construct the curl command
-                        curl_command = f'curl -sG --data-urlencode "db={db_name}" --data-urlencode "q={query}" {url}'
+                            # Construct the curl command
+                            curl_command = f'curl -sG --data-urlencode "db={db_name}" --data-urlencode "q={query}" {url}'
 
-                        # Execute the curl command using os.popen to capture output
-                        try:
-                            output = os.popen(curl_command).read()
+                            # Execute the curl command using os.popen to capture output
+                            try:
+                                output = os.popen(curl_command).read()
+                                response_data = json.loads(output)
+                                for result in response_data["results"]:
+                                    for series in result.get("series", []):
+                                        for entry in series.get("values", []):
+                                            name = series.get("name", "")
+                                            if name:
+                                                unique_measurement_names.add(name)
 
-                            # Parse the JSON response
-                            response_data = json.loads(output)
-                            for result in response_data["results"]:
-                                for series in result.get("series", []):
-                                    for entry in series.get("values", []):
-                                        name = series.get("name", "")
-                                        if name:
-                                            unique_measurement_names.add(name)  # Add unique names
+                            except Exception as e:
+                                print(f"Error executing curl command: {e}")
+                        
+                        new_lines.append(line.strip())  # Keep the line with \w* measurement
+                        
+                    else:
+                        new_lines.append(line.strip())  # Remove extra whitespace
 
-                        except Exception as e:
-                            print(f"Error executing curl command: {e}")
+                new_lines.extend(unique_measurement_names)  # Append unique names
 
-            # Write the unique measurement names to the metric file
-            with open(file_name, 'w') as metric_file:
-                metric_file.write("\n".join(unique_measurement_names))
+                # Write the updated lines back to the file
+                with open(file_name, 'w') as metric_file:
+                    metric_file.write("\n".join(filter(None, new_lines)))
+
                 processed_files.append(file_name)  # Add to processed files list
+        
         except FileNotFoundError:
             print(f"File '{file_name}' not found.")
+        
     else:
         print("======================================================")
         print(f"Skipping metric file: {file_name} (no processing)")
@@ -91,8 +100,21 @@ for file_name in metric_files:
 print("======================================================")
 print("Processed files:",",".join(processed_files))
 print("======================================================")
+# Read files, remove repeated names, and write back
+for file_name in processed_files:
+    try:
+        with open(file_name, 'r') as file:
+            lines = file.readlines()
+            unique_lines = list(set(line.strip() for line in lines if not re.search(r'\\w\*', line)))
+        
+        with open(file_name, 'w') as file:
+            file.write("\n".join(filter(None, unique_lines)))
+    except FileNotFoundError:
+        print(f"File '{file_name}' not found.")
 
-# Call the other bash script with the input file names as an argument
-other_script_command = f'./status-reporter.sh {input_file_names}'
+# Create a comma-separated string of processed file names
+processed_file_names = ",".join(processed_files)
+
+# Call the other bash script with the processed file names as an argument
+other_script_command = f'./status-reporter.sh {sys.argv[1]}'
 os.system(other_script_command)
-
